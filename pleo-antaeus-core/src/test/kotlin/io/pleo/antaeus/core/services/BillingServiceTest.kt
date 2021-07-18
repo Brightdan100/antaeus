493a09ee4b62534a5400dc487dc09eadb60b9db0
Daniel Itsuokor <danielitsuokor@Daniels-MacBook-Pro-2.local>
2021-07-18 21:08:42 +0100

    added testing

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -1,26 +1,38 @@
 package io.pleo.antaeus.core.services
 
+import io.mockk.*
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.core.external.PaymentProvider
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 
-internal class BillingServiceTest {
+class BillingServiceTest {
+    private val pendingInvoice: Invoice =  createPendingInvoice()
 
-//    var billingService = null
-//    val paymentProvider: PaymentProvider
-//    val invoiceService: InvoiceService
-    @BeforeEach
-    fun setUp() {
+    private val paymentProvider = mockk<PaymentProvider> {}
+
+    private val invoiceService = mockk<InvoiceService> {
+        every { fetchAllPendingInvoices() } returns listOf(pendingInvoice)
     }
 
-    @AfterEach
-    fun tearDown() {
+    private val billingService = BillingService(paymentProvider = paymentProvider, invoiceService = invoiceService)
+
+    @Test
+    fun `chargePendingInvoices when the payment provider returns true for charge`() {
+        every { paymentProvider.charge(pendingInvoice) } returns true
+        every{ invoiceService.updateStatus(InvoiceStatus.PAID, pendingInvoice) } returns pendingInvoice.id
+
+        billingService.chargePendingInvoices()
     }
 
     @Test
-    fun chargePendingInvoices() {
+    fun `chargePendingInvoices when the payment provider returns false for charge`() {
+        every { paymentProvider.charge(pendingInvoice) } returns false
+
+        billingService.chargePendingInvoices()
+
+        verify(inverse = true) { invoiceService.updateStatus(InvoiceStatus.PAID, pendingInvoice) }
     }
 }
